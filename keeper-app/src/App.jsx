1b6960f9b9ac67aebbcf2260c0e48cd51f22a684
u7546803 <u7546803@anu.edu.au>
2024-10-27 01:38:36 +1100

    Done the basic structure of the react App

---
+++ keeper-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Heading from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note.jsx";
+import './styles/css';
+
+function App() {
+    return <div><Heading /> <Note /> <Footer /> </div>;
+}
+
+export default App;
---
+++ keeper-app/src/Note.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Note() {
+
+  return (
+    <div className='note'>
+        <h1>Title</h1>
+        <p>content</p>
+    </div>
+  );
+}
+
+export default Note;
