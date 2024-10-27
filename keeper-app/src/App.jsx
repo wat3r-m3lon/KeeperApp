e61f5c776e4a0a56e697cf686a0929f4fe522a52
u7546803 <u7546803@anu.edu.au>
2024-10-27 15:00:51 +1100

    Remove wrong css import

--- keeper-app/src/App.jsx
+++ keeper-app/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Heading from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
-import './styles/css';
 
 function App() {
     return <div><Heading /> <Note /> <Footer /> </div>;
